1725f9df480f67a6e7772e0268f9c7678bfec313
RyanAdamGonzalez1996 <ryangonzalezportfolio@gmail.com>
2021-02-28 21:17:22 -0500

    Finished framework for JS application, will update when we have working model.

--- static/js/app.js
+++ static/js/app.js
@@ -1,42 +1,54 @@
-/*var test = d3.selectAll("li");
-console.log(test);
+// Initialize the button instance
+var button = d3.select("#filter-btn");
 
-var test2 = d3.select("#streammovie");
-console.log(test2);
+// Initialize the form instance
+var form = d3.select("#form");
 
-var test3 = test2.property("value");
-console.log(test3);
+// Create Event handlers
+button.on("click", runEvent);
+form.on("submit", runEvent);
 
-var test4 = d3.select("li");
-console.log(test4);
+// Creates the output area to display monthly charges
+function createOutput(){
 
-var test5 = test4.select("select");
-console.log(test5);*/
+    // Output results as an alert
+    window.alert("test");
+    var testOutput = document.getElementById("output");
+    testOutput.value = "test";
+    console.log(testOutput);
+};
 
-//Iterate through each list item
+// function to listen to event and read in values
+function runEvent(){
 
-    //Save each property("value") to an object
+    // Prevent Page from Refreshing
+    d3.event.preventDefault();
 
-// if I cant get the loop
-// jsut do it manually as it is a set value of properties
-var gender = d3.select("#gender").property("value");
-var retired = d3.select("#retired").property("value");
-var partner = d3.select("#partner").property("value");
-var dependents = d3.select("#dependents").property("value");
-var tenure = d3.select("#tenure").property("value");
-var telephone = d3.select("#telephone").property("value");
-var lines = d3.select("#lines").property("values");
-var internet = d3.select("#internet").property("values");
-var security = d3.select("#security").property("values");
-var backup = d3.select("#backup").property("value");
-var protection = d3.select("#protection").property("value");
-var techsupport = d3.select("#techsupport").property("value");
-var streamtv = d3.select("#streamtv").property("value");
-var streammovie = d3.select("#streammovie").property("value");
-var contract = d3.select("#contract").property("value");
-var billing = d3.select("#billing").property("value");
-var method = d3.select("#method").property("value");
-var currentpayment = d3.select("#currentpayment").property("value");
+    // Declare a an object to save each value in survey
+    var objTest = {};
 
-// save the values as individual cases
-// will see if individual values or object works best
+    // Save values into the object
+    objTest.gender = d3.select("#gender").property("value");
+    objTest.retired = d3.select("#retired").property("value");
+    objTest.partner = d3.select("#partner").property("value");
+    //var dependents = d3.select("#dependents").property("value");
+    objTest.tenure = d3.select("#tenure").property("value");
+    objTest.telephone = d3.select("#telephone").property("value");
+    objTest.lines = d3.select("#lines").property("values");
+    objTest.internet = d3.select("#internet").property("values");
+    objTest.security = d3.select("#security").property("values");
+    objTest.backup = d3.select("#backup").property("value");
+    objTest.protection = d3.select("#protection").property("value");
+    objTest.techsupport = d3.select("#techsupport").property("value");
+    objTest.streamtv = d3.select("#streamtv").property("value");
+    objTest.streammovie = d3.select("#streammovie").property("value");
+    objTest.contract = d3.select("#contract").property("value");
+    objTest.billing = d3.select("#billing").property("value");
+    objTest.method = d3.select("#method").property("value");
+    objTest.currentpayment = d3.select("#currentpayment").property("value");
+
+    console.log(objTest);
+
+    // Call the create Output function
+    createOutput(objTest);
+}
