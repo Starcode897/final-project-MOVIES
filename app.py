761c923aa2b3c016c0c2a0ccaea5e62ca76603fd
Starcode897 <69259854+Starcode897@users.noreply.github.com>
2021-03-02 13:10:02 -0500

    I think I understand how the schema works.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
-#model = pickle.load(open('iri.pkl', 'rb'))
+model = pickle.load(open('model_pickle', 'rb'))
 
 app = Flask(__name__)
 
@@ -13,16 +13,19 @@ def man():
     return render_template('predict.html')
 
 
+# The app grabs the info submitted from predict.html, and passes it into the model as an array.
+# According to flask, the model doesn't exist.
+
 @app.route('/predict', methods=['POST'])
 def home():
-    data1 = request.form['a']
-    data2 = request.form['b']
-    data3 = request.form['c']
-    data4 = request.form['d']
+    data1 = request.form['gen']
+    data2 = request.form['retire']
+    data3 = request.form['pard']
+    data4 = request.form['depend']
     arr = np.array([[data1, data2, data3, data4]])
     pred = model.predict(arr)
     # return pred
-    return render_template('after.html', data=pred)
+    return render_template('after_predict.html', data=pred)
 
 
 if __name__ == "__main__":
