9833059ad8ffc4ec13d7791d1a9040363f8b7f20
Starcode897 <69259854+Starcode897@users.noreply.github.com>
2021-03-02 19:19:55 -0500

    The model WORKS GODDAMMIT!!

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
-model = pickle.load(open('/Users/tylerbrown/Documents/Data Science bootcamp/iris_html/model_pickle', 'rb'))
+model = pickle.load(open('new.pkl', 'rb'))
 
 app = Flask(__name__)
 
@@ -24,19 +24,18 @@ def home():
     data4 = request.form['dependents']
     data5 = request.form['tenure']
     data6 = request.form['telephone']
-    data7 = request.form['lines']
-    data8 = request.form['internet']
+    data7 = request.form['DSL']
+    data8 = request.form['Fiber']
     data9 = request.form['security']
     data10 = request.form['backup']
     data11 = request.form['protection']
     data12 = request.form['techsupport']
     data13 = request.form['streamtv']
     data14 = request.form['streammovie']
-    data15 = request.form['contract']
-    data16 = request.form['billing']
-    data17 = request.form['currentpayment']
+    data15 = request.form['billing']
+    #data15 = request.form['currentpayment']
     arr = np.array([[data1, data2, data3, data4, data5, data6, data7,
-    data8, data9, data10, data11, data12, data13, data14, data15, data16, data17]])
+    data8, data9, data10, data11, data12, data13, data14, data15]])
     pred = model.predict(arr)
     # return pred
     return render_template('after_predict.html', data=pred)
